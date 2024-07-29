1ecbbac45efddc42908001ee1ec9cfc5fdc7e57d
chris <beanfun4961261@gmail.com>
2024-07-29 23:40:28 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/0729vite/',
+  base: '/vite3/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
